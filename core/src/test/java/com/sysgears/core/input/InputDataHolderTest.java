7be0fe9be01b304078b538e0a07a3a948ed16243
Yevgen Goliuk <yevgeniy.golyuk@gmail.com>
2016-12-29 17:56:40 +0200

    begin to add tests

--- core/src/test/java/com/sysgears/core/input/InputDataHolderTest.java
+++ core/src/test/java/com/sysgears/core/input/InputDataHolderTest.java
@@ -1,12 +1,12 @@
 package com.sysgears.core.input;
 
 import org.apache.commons.cli.CommandLine;
+import org.easymock.EasyMock;
 
 /**
  * @author Yevgen Goliuk
  */
 public class InputDataHolderTest {
-
-//    CommandLine cml = new CommandLine();
+    CommandLine cml = EasyMock.createMock(CommandLine.class);
 //    InputDataHolder dataHolder = new InputDataHolder(cml);
 }
