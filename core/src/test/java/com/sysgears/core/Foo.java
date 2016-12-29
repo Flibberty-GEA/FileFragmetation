0772c338eab0948b08837a960c5a0e1d12a32d13
Yevgen Goliuk <yevgeniy.golyuk@gmail.com>
2016-12-29 16:44:12 +0200

    last version

---
+++ core/src/test/java/com/sysgears/core/Foo.java
@@ -0,0 +1,8 @@
+package com.sysgears.core;
+
+/**
+ * @author Yevgen Goliuk
+ */
+public class Foo {
+//public static final Logger log = LogManager.getLogger(Foo.class);
+}
---
+++ core/src/test/java/com/sysgears/core/input/InputDataHolderTest.java
@@ -0,0 +1,12 @@
+package com.sysgears.core.input;
+
+import org.apache.commons.cli.CommandLine;
+
+/**
+ * @author Yevgen Goliuk
+ */
+public class InputDataHolderTest {
+
+//    CommandLine cml = new CommandLine();
+//    InputDataHolder dataHolder = new InputDataHolder(cml);
+}
